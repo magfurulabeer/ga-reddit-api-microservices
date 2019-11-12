13ae1f4c1c131a02880c91c8b1d127d1a5e9b0ff
gkopplin <grant.kopplin@gmail.com>
2019-11-12 15:07:43 -0500

    Add initial code for auth config in users service

--- users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
+++ users-api/src/main/java/com/example/usersapi/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.usersapi.repository;
 
 import com.example.usersapi.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,7 @@ import java.util.List;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-//    public List<User> findByFirstNameContainsOrLastNameContains(String firstName, String lastName);
+
+    @Query("FROM User u WHERE u.username = ?1")
+    public User getUserByUsername(String username);
 }
