db3614fe13d22696d1a66f478fc029089d2e7687
gkopplin <grant.kopplin@gmail.com>
2019-11-13 14:16:17 -0500

    Add unique constraint for users

--- users-api/src/main/java/com/example/usersapi/model/User.java
+++ users-api/src/main/java/com/example/usersapi/model/User.java
@@ -9,10 +9,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password", nullable = false)
