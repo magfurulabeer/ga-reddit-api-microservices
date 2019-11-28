2f4181f0e8248c8035c978a55e0639b596687906
gkopplin <grant.kopplin@gmail.com>
2019-11-28 09:37:08 -0500

    Add happy paths for post controller

--- posts-api/src/test/java/com/example/postsapi/controller/PostControllerTest.java
+++ posts-api/src/test/java/com/example/postsapi/controller/PostControllerTest.java
@@ -13,12 +13,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -56,20 +58,23 @@ public class PostControllerTest {
                 "\"body\":\"" + description + "\"}";
     }
 
-    @Test
-    public void createPost_Post_Success() throws Exception {
-
+    private ResultActions createPostHelper() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .post("/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .header("username", "username")
                 .content(createPostInJson("title", "description", "username"));
 
-
         when(postService.createPost((any()), any())).thenReturn(post);
 
-        MvcResult result = mockMvc.perform(requestBuilder)
-                .andExpect(status().isOk())
+        return mockMvc.perform(requestBuilder);
+    }
+
+    @Test
+    public void createPost_Post_Success() throws Exception {
+        ResultActions result = createPostHelper();
+
+        result.andExpect(status().isOk())
                 .andExpect(content().json("{\"id\":1,\"title\":\"title\",\"description\":\"description\",\"user\":{\"username\":\"username\"}}"))
                 .andReturn();
 
@@ -91,23 +96,51 @@ public class PostControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().json("[{\"id\":1,\"title\":\"title\",\"description\":\"description\",\"user\":{\"username\":\"username\"}}]"))
                 .andReturn();
-
-        System.out.println(result.getResponse().getContentAsString());
     }
 
     @Test
     public void deletePost_Post_Success() throws Exception {
 
+        createPostHelper();
+
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .delete("/post/1")
+                .delete("/1")
                 .contentType(MediaType.APPLICATION_JSON);
 
-
-        when(postService.deletePost((any()))).thenReturn(HttpStatus.OK);
+        when(postService.deletePost(anyLong())).thenReturn(HttpStatus.OK);
 
         MvcResult result = mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
-                .andExpect(content().json("{\"postId\":1,\"title\":\"title\",\"body\":\"body\",\"author\":{\"userId\":1,\"username\":\"name3\",\"password\":\"pass\",\"email\":\"name@domain.com\",\"address\":null,\"mobile\":null,\"addlEmail\":null},\"comments\":null}"))
+                .andReturn();
+    }
+
+    @Test
+    public void postWithPostIdExists_False_Success() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/1")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        when(postService.searchById(anyLong())).thenReturn(null);
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("false"))
+                .andReturn();
+    }
+
+    @Test
+    public void postWithPostIdExists_True_Success() throws Exception {
+        ResultActions action = createPostHelper();
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/1")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        when(postService.searchById(anyLong())).thenReturn(post);
+
+        MvcResult result = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().string("true"))
                 .andReturn();
     }
 }
