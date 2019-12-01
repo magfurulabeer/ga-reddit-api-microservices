02d525b83493c5e822746bdbb77a6fce931549c4
gkopplin <grant.kopplin@gmail.com>
2019-12-01 12:46:54 -0500

    Set default profile to test and drop dummy test

--- users-api/src/test/java/com/example/usersapi/UsersApiApplicationTests.java
+++ users-api/src/test/java/com/example/usersapi/UsersApiApplicationTests.java
@@ -2,18 +2,12 @@ package com.example.usersapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class UsersApiApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void dummyTest() {
-		assertEquals(true, true);
 	}
 
 }
