508110338801ab8308bacc2c73dc70deedd78f1f
gkopplin <grant.kopplin@gmail.com>
2019-12-02 12:50:01 -0500

    Fix posts api coverage issue

--- posts-api/src/test/java/com/example/postsapi/controller/PostControllerTests.java
+++ posts-api/src/test/java/com/example/postsapi/controller/PostControllerTests.java
@@ -8,13 +8,11 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
