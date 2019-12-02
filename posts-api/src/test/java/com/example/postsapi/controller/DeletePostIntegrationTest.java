9dd11a9211ed55b98e39f36ae4f3abc23d4a43f8
magfurulabeer <magfurulabeer@gmail.com>
2019-12-02 15:54:28 -0500

    Fix integration test

--- posts-api/src/test/java/com/example/postsapi/controller/DeletePostIntegrationTest.java
+++ posts-api/src/test/java/com/example/postsapi/controller/DeletePostIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.example.postsapi.controller;
 
+import com.example.postsapi.model.Post;
 import com.example.postsapi.mq.DummyReceiver;
+import com.example.postsapi.service.PostService;
 import com.google.common.io.Files;
 import org.apache.qpid.server.Broker;
 import org.apache.qpid.server.BrokerOptions;
@@ -9,6 +11,7 @@ import org.junit.Test;
 import org.junit.rules.ExternalResource;
 import org.junit.runner.RunWith;
 import org.junit.runner.Runner;
+import org.mockito.InjectMocks;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +21,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.Properties;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,21 +34,14 @@ public class DeletePostIntegrationTest {
     @Value("${spring.rabbitmq.port}")
     private String rabbitmqPort;
 
-//    @Autowired
-//    private Queue queue;
-
-
-    public static final String QPID_CONFIG_LOCATION = "src/test/resources/qpid-config.json";
-    public static final String APPLICATION_CONFIG_LOCATION = "src/test/resources/application.properties";
-
-    @MockBean
-    private Runner runner;
-
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @Autowired
     private DummyReceiver receiver;
+
+    @Autowired
+    PostService postService;
 
     @ClassRule
     public static final ExternalResource resource = new ExternalResource() {
@@ -52,7 +50,7 @@ public class DeletePostIntegrationTest {
         @Override
         protected void before() throws Throwable {
             Properties properties = new Properties();
-            properties.load(new FileInputStream(new File(APPLICATION_CONFIG_LOCATION)));
+            properties.load(new FileInputStream(new File("src/test/resources/application.properties")));
             String amqpPort = properties.getProperty("spring.rabbitmq.port");
             File tmpFolder = Files.createTempDir();
             String userDir = System.getProperty("user.dir").toString();
@@ -62,28 +60,34 @@ public class DeletePostIntegrationTest {
             brokerOptions.setConfigProperty("qpid.work_dir", tmpFolder.getAbsolutePath());
             brokerOptions.setConfigProperty("qpid.amqp_port", amqpPort);
             brokerOptions.setConfigProperty("qpid.home_dir", homePath);
-            brokerOptions.setInitialConfigurationLocation(homePath + "/" + QPID_CONFIG_LOCATION);
+            brokerOptions.setInitialConfigurationLocation(homePath + "/src/test/resources/qpid-config.json");
             broker.startup(brokerOptions);
         }
-
 
         @Override
         protected void after() {
             broker.shutdown();
         }
-
     };
 
 
     @Test
-    public void testWithFirstReceiverRoutingKey() throws Exception {
+    public void simpleReceive_Message_Success() throws Exception {
         rabbitTemplate.convertAndSend("queue1", "Test 1");
         Thread.sleep(5000);
         assertThat(receiver.getId()).isEqualTo("Test 1");
-        rabbitTemplate.convertAndSend("queue1", "Test 2");
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void deletePost_HttpStatus_Success() throws IOException, InterruptedException {
+        Post p = new Post();
+        p.setTitle("title");
+        p.setDescription("description");
+        postService.createPost(p, "username");
+        postService.deletePost(1L);
         Thread.sleep(5000);
-        assertThat(receiver.getId()).isEqualTo("Test 2");
-//        assertThat(true).isEqualTo(true);
+        assertThat(receiver.getId()).isEqualTo("1");
+        postService.searchById(1L);
     }
 
 }
