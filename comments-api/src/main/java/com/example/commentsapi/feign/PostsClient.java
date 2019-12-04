20e08cafe183410640f639bf52457e6fe0875a94
magfurulabeer <magfurulabeer@gmail.com>
2019-12-04 09:19:40 -0500

    Update projects for deployment

--- comments-api/src/main/java/com/example/commentsapi/feign/PostsClient.java
+++ comments-api/src/main/java/com/example/commentsapi/feign/PostsClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "PostsClient", url="http://posts-api:8082")
+@FeignClient(name = "PostsClient", url="https://posts-api.cfapps.io/")
 public interface PostsClient {
     @GetMapping(value = "/{postId}")
     public boolean postWithPostIdExists(@PathVariable long postId);
