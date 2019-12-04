9d0503d34b7505e70ea85f078081270f2ec32796
magfurulabeer <magfurulabeer@gmail.com>
2019-12-04 12:14:15 -0500

    Add javadoc comments

--- users-api/src/main/java/com/example/usersapi/service/UserRoleService.java
+++ users-api/src/main/java/com/example/usersapi/service/UserRoleService.java
@@ -7,6 +7,12 @@ import org.springframework.http.HttpStatus;
 import java.util.Collection;
 
 public interface UserRoleService {
+    /**
+     * Creates and saves the given user to the database
+     *
+     * @param userRole the UserRole object that needs to be saved to the database
+     * @return the saved UserRole if successful
+     */
     public UserRole createUserRole(UserRole userRole);
 
     public Collection<UserRole> searchByUserId(long userId) throws EntityNotFoundException;
--- users-api/src/main/java/com/example/usersapi/service/UserService.java
+++ users-api/src/main/java/com/example/usersapi/service/UserService.java
@@ -7,17 +7,42 @@ import org.springframework.http.HttpStatus;
 
 public interface UserService {
 
+    /**
+     * Gets a list of all users
+     *
+     * @return an iterable containing all users
+     */
     public Iterable<User> getAll();
 
+    /**
+     * Returns the user with the given id
+     *
+     * @param id  the id of the user
+     * @return the User with the given id
+     */
     public User searchById(long id);
 
-//    public Iterable<User> searchByName(String name);
-
+    /**
+     * Deletes the user with the given id
+     *
+     * @param id  the id of the user
+     * @return an HttpStatus representing the success or failure of the operation
+     */
     public HttpStatus deleteUser(long id);
 
+    /**
+     * Creates and saves the given user to the database
+     *
+     * @param user the user object that needs to be saved to the database
+     * @return the a JWT to be used for authentication purposes
+     */
     public String createUser(User user) throws DuplicateUserException;
 
+    /**
+     * Logs in using username and password
+     *
+     * @param user the user object that needs to be saved to the database
+     * @return the a JWT to be used for authentication purposes
+     */
     public String login(User user) throws InvalidCredentialsException;
-
-//    public HttpStatus updateUser(long id, User userRequest);
 }
